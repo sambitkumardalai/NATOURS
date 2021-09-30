3f1d6e176daa61d89da45e90825544e79291bed1
sambit <sambitskd3@gmail.com>
2021-09-30 07:34:15 +0530

    added filter functionality in api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,7 +21,37 @@ exports.createTour = async (req, res) => {
 // get all the tours
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Build query
+    // 1.a) filter query
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((element) => {
+      delete queryObj[element];
+    });
+
+    // 1.b) advance filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // execute query
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2)sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    const tours = await query;
     res.status(200).json({
       status: 'success',
       results: tours.length,
