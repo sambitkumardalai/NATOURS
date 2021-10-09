1af7509088d009076cdd79ff9f8b11dbfa93773b
sambit <sambitskd3@gmail.com>
2021-10-09 15:02:21 +0530

    modified import model name and new tour data migrated from json to mongodb

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 // const Tour = require('./../../models/tourModel');
-const Tour = require('./../../Model/tourModel');
+const Tour = require('./../../models/tourModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -20,9 +20,7 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 // READ JSON FILE
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // IMPORT DATA INTO DB
 const importData = async () => {
