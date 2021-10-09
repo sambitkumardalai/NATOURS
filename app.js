adfbc366dcc5cdad1affb1e7283d72288563ac61
sambit <sambitskd3@gmail.com>
2021-10-09 06:18:14 +0530

    data sanitization implemented

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -9,16 +12,24 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 app.use(express.json());
 
-// 1) Middleware
+// 1) golbal Middlewares
+// app.use(helmet());
+// data sanitization against nosql query injection
+// app.use(mongoSanitize());
+// app.use(xss());
+// logging in to development environment
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit requests from same API
 const limiter = rateLimit({
-  max: 2,
+  max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests😵..Please try again in an hour',
 });
 app.use('/api', limiter);
+
 app.use((req, res, next) => {
   req.requsetTime = new Date().toISOString();
   // console.log(req.headers);
@@ -36,6 +47,7 @@ app.all('*', (req, res, next) => {
 
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+// body parser, reading data from body into req.body
 
 app.use(globalErrorHandler);
 module.exports = app;
