3b4411d51837400482ccf5b74d19c8e5b4dad89b
sambit <sambitskd3@gmail.com>
2021-10-11 05:15:18 +0530

    implemented nested route for review

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
-
+router.use('/:tourId/reviews', reviewRouter);
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
@@ -26,5 +28,11 @@ router
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
   );
-
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
+  );
 module.exports = router;
