85d863a33213043d89223cc5cf406a2debafd6cf
sambit <sambitskd3@gmail.com>
2021-10-18 05:41:40 +0530

    binded tour data to cards in view page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,15 @@
-exports.getOverview = (req, res) => {
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // get tour data from collection
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
