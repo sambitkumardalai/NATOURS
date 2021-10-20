ab56142ba9491b3ab4f095fb2310acfe15b2b41e
sambit <sambitskd3@gmail.com>
2021-10-20 05:50:20 +0530

    mapbox library for map integrated

---
+++ public/js/mapbox.js
@@ -0,0 +1,43 @@
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2FtYml0c2tkMyIsImEiOiJja3V5cGcwcTEwcWJrMnZxcWVlZTg4d3Q5In0.2HWnoEZnNVjfBINDt5G_Yw';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  scrollZoom: false,
+  //   center: [-118.113491, 34.111745],
+  //   zoom: 4,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // add popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.Day}: ${loc.description}</p>`)
+    .addTo(map);
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 200,
+    right: 200,
+  },
+});
