9ba6448e4ce86eef0141367bd557cdadd794aaee
sambit <sambitskd3@gmail.com>
2021-10-20 07:29:31 +0530

    integrated axios for http request and used cookie parser for jwt token

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -11,7 +11,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
@@ -27,3 +27,15 @@ exports.getTour = catchAsync(async (req, res) => {
       tour,
     });
 });
+
+exports.getLoginForm = (req, res) => {
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "connect-src 'self' https://cdnjs.cloudflare.com"
+    )
+    .render('login', {
+      title: 'Log into your account',
+    });
+};
