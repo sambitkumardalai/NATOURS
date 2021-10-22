b4e8e161bd2c6ef3f2943d072e732b20abb987c2
sambit <sambitskd3@gmail.com>
2021-10-22 07:44:20 +0530

    added parcel package to manage module and login part completed

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+import '@babel/polyfill';
+import { displayMap } from './mapbox';
+import { login } from './login';
+
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form');
+
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,43 +1,43 @@
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
-mapboxgl.accessToken =
-  'pk.eyJ1Ijoic2FtYml0c2tkMyIsImEiOiJja3V5cGcwcTEwcWJrMnZxcWVlZTg4d3Q5In0.2HWnoEZnNVjfBINDt5G_Yw';
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  scrollZoom: false,
-  //   center: [-118.113491, 34.111745],
-  //   zoom: 4,
-});
+export const displayMap = (locatios) => {
+  mapboxgl.accessToken =
+    'pk.eyJ1Ijoic2FtYml0c2tkMyIsImEiOiJja3V5cGcwcTEwcWJrMnZxcWVlZTg4d3Q5In0.2HWnoEZnNVjfBINDt5G_Yw';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    scrollZoom: false,
+    //   center: [-118.113491, 34.111745],
+    //   zoom: 4,
+  });
 
-const bounds = new mapboxgl.LngLatBounds();
+  const bounds = new mapboxgl.LngLatBounds();
 
-locations.forEach((loc) => {
-  const el = document.createElement('div');
-  el.className = 'marker';
+  locations.forEach((loc) => {
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom',
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom',
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
 
-  // add popup
-  new mapboxgl.Popup({
-    offset: 30,
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>Day ${loc.Day}: ${loc.description}</p>`)
-    .addTo(map);
-  bounds.extend(loc.coordinates);
-});
+    // add popup
+    new mapboxgl.Popup({
+      offset: 30,
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.Day}: ${loc.description}</p>`)
+      .addTo(map);
+    bounds.extend(loc.coordinates);
+  });
 
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 200,
-    right: 200,
-  },
-});
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 200,
+      right: 200,
+    },
+  });
+};
