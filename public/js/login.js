55b07ef60cea896fd9d5a2fe2bc2331ec1e424af
sambit <sambitskd3@gmail.com>
2021-10-23 10:29:11 +0530

    user data updated using api

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,21 @@
+/* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
 
 export const login = async (email, password) => {
-  // console.log(email, password);
   try {
     const res = await axios({
       method: 'POST',
       url: 'http://127.0.0.1:3000/api/v1/users/login',
       data: {
         email,
-        password,
-      },
+        password
+      }
     });
+
     if (res.data.status === 'success') {
-      showAlert('success', 'Logged in successfully');
-      setTimeout(() => {
+      showAlert('success', 'Logged in successfully!');
+      window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
@@ -27,10 +28,11 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+      url: 'http://127.0.0.1:3000/api/v1/users/logout'
     });
     if ((res.data.status = 'success')) location.reload(true);
-  } catch (error) {
-    showAlert('error', 'Error logging out !');
+  } catch (err) {
+    console.log(err.response);
+    showAlert('error', 'Error logging out! Try again.');
   }
 };
