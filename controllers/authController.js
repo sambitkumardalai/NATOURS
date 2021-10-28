c91499ce52d230210e55f563c7d1e10c3b84158b
sambit <sambitskd3@gmail.com>
2021-10-28 07:26:46 +0530

    email bug for fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,8 +44,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     role: req.body.role,
     photo: req.body.photo,
   });
-  // const url = `${req.protocol}://${req.get('host')}/me`;
-  const url = `${req.protocol}://localhost:3000/me`;
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  // const url = `${req.protocol}://localhost:3000/me`;
   console.log(url);
   await new Email(newUser, url).sendWelcome();
   const token = signToken(newUser._id);
@@ -158,25 +158,17 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword/${resetToken}`;
-
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
-
   try {
-    // await sendEmail({
-    //   email: user.email,
-    //   subject: 'Your password reset token (valid for 10 min)',
-    //   message,
-    // });
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+    await new Email(user, resetURL).sendPasswordReset();
 
     res.status(200).json({
       status: 'success',
       message: 'Token sent to email!',
     });
   } catch (err) {
-    console.log(err);
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
     await user.save({ validateBeforeSave: false });
@@ -187,6 +179,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     );
   }
 });
+
 exports.resetPassword = async (req, res, next) => {
   // get user based on the token
   const hashedToken = crypto
