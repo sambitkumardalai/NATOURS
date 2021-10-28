6e78c799cc95ffb9f65a925b141e052c433bc1f3
sambit <sambitskd3@gmail.com>
2021-10-28 07:26:46 +0530

    email bug for fixed

--- utils/email.js
+++ utils/email.js
@@ -1,20 +1,27 @@
 const nodemailer = require('nodemailer');
 const pug = require('pug');
-// const htmltotext = require('html-to-text');
-const { htmlToText } = require('html-to-text');
+const htmlToText = require('html-to-text');
 
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Sambit Dalai <${process.env.EMAIL_FROM}>`;
+    this.from = `Jonas Schmedtmann <${process.env.EMAIL_FROM}>`;
   }
+
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      return 1;
+      // Sendgrid
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
     }
-    // 1) Create a transporter
+
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
@@ -24,20 +31,37 @@ module.exports = class Email {
       },
     });
   }
+
+  // Send the actual email
   async send(template, subject) {
-    // 1)render a html based on a pug template
-    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`);
-    // 2) Define the email options
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+
+    // 2) Define email options
     const mailOptions = {
       from: this.from,
       to: this.to,
       subject,
-      text: htmlToText(html),
+      html,
+      text: htmlToText.fromString(html),
     };
-    // create a transport and send email
+
+    // 3) Create a transport and send email
     await this.newTransport().sendMail(mailOptions);
   }
+
   async sendWelcome() {
-    await this.send('Welcome', 'Welcome to the natours family');
+    await this.send('welcome', 'Welcome to the Natours Family!');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)'
+    );
   }
 };
