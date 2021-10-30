97c972722d909b8de29d1b3af1680b1de2af5ec7
sambit <sambitskd3@gmail.com>
2021-10-30 11:38:23 +0530

    booking page completed with api integration

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
@@ -68,5 +69,15 @@ exports.updateUserData = catchAsync(async (req, res, next) => {
   res.status(200).render('account', {
     title: 'Your account',
     user: updatedUser,
+  });
+});
+
+exports.getMyTours = catchAsync(async (req, res, next) => {
+  const bookings = await Booking.find({ user: req.user.id });
+  const tourIDs = bookings.map((el) => el.tour);
+  const tours = await Tour.find({ _id: { $in: tourIDs } });
+  res.status(200).render('overview', {
+    title: 'My tours',
+    tours,
   });
 });
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9411,7 +9411,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64909" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62911" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
