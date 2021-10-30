02200ee0fe7b7106ae0abbad6e8308326721d741
sambit <sambitskd3@gmail.com>
2021-10-30 12:27:34 +0530

    file bundling completed

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -5,9 +5,7 @@ const stripe = Stripe(
 );
 export const bookTour = async (tourId) => {
   try {
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     // console.log(session);
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id,
