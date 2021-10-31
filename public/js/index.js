0b070456b43dd8037d0b81e9123798fa215c7218
sambit <sambitskd3@gmail.com>
2021-10-31 11:14:53 +0530

    implemented stripe messages

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,8 @@ import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
+
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
@@ -65,3 +67,6 @@ if (bookBtn) {
     bookTour(tourId);
   });
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alert) showAlert('success', alertMessage, 20);
